fa3a44ae6f0cc6dcc89af2f4cbd7cd75ec7234b7
SonMyeongJin <smj2802@naver.com>
2025-01-13 18:39:56 +0900

    Initial Commit

---
+++ DaeBakTests/DaeBakTests.swift
@@ -0,0 +1,16 @@
+//
+//  DaeBakTests.swift
+//  DaeBakTests
+//
+//  Created by 손명진 on 1/13/25.
+//
+
+import Testing
+
+struct DaeBakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
