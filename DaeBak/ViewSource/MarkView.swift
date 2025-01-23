04157d903e73f036e9871c6cbfd3c7628b248371
SonMyeongJin <smj2802@naver.com>
2025-01-23 19:06:32 +0900

    MarkView 생성 : 마크 그림 누르면 리스트로 넘어가도록

---
+++ DaeBak/ViewSource/MarkView.swift
@@ -0,0 +1,36 @@
+//
+//  MarkView.swift
+//  DaeBak
+//
+//  Created by 손명진 on 1/23/25.
+//
+
+import SwiftUI
+
+struct MarkView: View {
+    let artist: Artist
+    
+    var body: some View {
+        VStack {
+            Image(artist.rawValue)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 100, height: 100)
+                .clipShape(Circle())
+                .overlay(
+                    Circle().stroke(Color.gray, lineWidth: 6)
+                )
+                .shadow(radius: 5)
+            Text(artist.rawValue)
+                .font(.headline)
+        }
+    }
+}
+
+#Preview {
+
+    MarkView(artist: .ASTRO)
+    MarkView(artist: .SEVENTEEN)
+    MarkView(artist: .BTS)
+}
+
