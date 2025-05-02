df31bbfb1dffe9015a60dbe0e6bcbd7379eae4b1
SonMyeongJin <smj2802@naver.com>
2025-05-02 21:08:41 +0900

    IPhone max 버전에서 메인페이지에 아티스트View 달라지는점 수정

--- DaeBak/MainView/ArtistListView.swift
+++ DaeBak/MainView/ArtistListView.swift
@@ -18,7 +18,10 @@ struct ArtistListView: View {
                 TitleView(title: "K-POP の 魅力")
                 
                 ScrollView {
-                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))], spacing: 50) {
+                    LazyVGrid(columns: [
+                        GridItem(.flexible(), spacing: 20),
+                        GridItem(.flexible(), spacing: 20)
+                    ], spacing: 50) {
                         ForEach(Artist.allCases) { artist in
                             NavigationLink(destination: ScriptListView(artist: artist)) {
                                 MarkView(artist: artist) // 기존에 정의된 아티스트 표시 뷰
